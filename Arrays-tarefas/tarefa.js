4e7baa790440198497f1ae505917ac5ff881521e
Thales971 <thales.t.silva6@aluno.senai.br>
2025-05-30 08:34:04 -0300

    feat: Adiciona novas tarefas ao arquivo tarefa.js

---
+++ Arrays-tarefas/tarefa.js
@@ -0,0 +1,10 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log("Antes do push");
+
+console.table(tarefas);
+
+tarefas.push ('Jogar warzone','Fazer cardio de 30min')
+
+console.table(tarefas)
+
