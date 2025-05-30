9f8e0a169338c84479ec3b21dbd7a0ed589e5cdb
Thales971 <thales.t.silva6@aluno.senai.br>
2025-05-30 08:43:10 -0300

    feat: Remove ultima tarefa ao arquivo tarefa.js

--- Arrays-tarefas/tarefa.js
+++ Arrays-tarefas/tarefa.js
@@ -1,5 +1,5 @@
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
-
+// push adiciona
 console.log("Antes do push");
 
 console.table(tarefas);
@@ -7,4 +7,20 @@ console.table(tarefas);
 tarefas.push ('Jogar warzone','Fazer cardio de 30min')
 
 console.table(tarefas)
+
+// pop remove
+
+console.log('--------------------------------');
+
+console.log
+("Antes do pop");
+console.table(tarefas);
+
+let estiloRemovido = tarefas.pop();
+
+console.log("Depois do pop: Ultima tarefa removida");
+
+console.table(tarefas)
+
+
 
